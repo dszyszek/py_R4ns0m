40f9cb4edac10c5a32627b3cf02273f3345fd7a7
Damian <37978110+dszyszek@users.noreply.github.com>
2019-05-21 10:24:13 +0200

    Aad decrypt function (cryptography module)

--- modules/cryptography.py
+++ modules/cryptography.py
@@ -33,3 +33,34 @@ def encrypt(key, file_path):
                 output_file.write(encrypted)
 
     os.remove(file_path)
+
+
+def decrypt(key_in, file_path):
+    key = normalize_key(key_in)
+
+    filename = ntpath.basename(file_path)
+
+    restored_name = filename[:-10]
+    chunk_size = 16*1024
+
+    with open(file_path, 'rb') as input_file:
+        restored_file_path = os.path.join(os.path.dirname(file_path), restored_name) # set up restored file path
+        file_size_info = int(input_file.read(16))
+        iv = input_file.read(16)
+
+        decipher = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(restored_file_path, 'wb') as output_file:
+
+            while True:
+                r = input_file.read(chunk_size)
+
+                if len(r) == 0:
+                    break
+
+                decrypted = decipher.decrypt(r)
+                output_file.write(decrypted)
+
+            output_file.truncate(file_size_info)
+
+    os.remove(file_path)
