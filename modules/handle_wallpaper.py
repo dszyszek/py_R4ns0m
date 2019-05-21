181c16ad59dab9b81c29af7c7c9ebc1e6d1a4a66
Damian <37978110+dszyszek@users.noreply.github.com>
2019-05-21 10:45:49 +0200

    Add change_wallpaper function (handle_wallpaper module)

--- modules/handle_wallpaper.py
+++ modules/handle_wallpaper.py
@@ -17,3 +17,39 @@ def recognize_platform():
         return 'linux'
     else:
         return 'unknown', os_platform
+
+
+def change_wallpaper():
+    system = recognize_platform()
+    path_to_img = normalize_path_name(sys._MEIPASS, 'frog.jpg')
+
+    if system == 'windows':
+        # Test windows background change
+        import ctypes
+
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_to_img, 0)
+
+    elif system == 'osx':
+        # Test osx background change
+
+        script = """
+
+        on run(x)
+            tell application "Finder" to set desktop picture to POSIX file x   
+        end run
+
+        """
+
+        args = [path_to_img]
+        p = subprocess.Popen(['osascript', '-'] + args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        p.communicate(script)
+
+    elif system == 'linux':
+        # Test linux background change
+        pass
+
+
+if __name__ == '__main__':
+    change_wallpaper()
